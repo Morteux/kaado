cf0b869101ee98bdb3534a2984a53bb5de86f624
Morteux <ignacio.melendezlapi@alum.uca.es>
2023-03-12 15:13:33 +0100

    Dictionary - All checkboxes

--- js/dictionary.js
+++ js/dictionary.js
@@ -63,7 +63,7 @@ var dictionary = {
     ],
 
 
-    "días de la semana": [
+    "semana": [
         { "latin": "días de la semana", "japanese": "ようび" },
         { "latin": "lunes", "japanese": "げつようび" },
         { "latin": "martes", "japanese": "かようび" },
@@ -75,7 +75,7 @@ var dictionary = {
     ],
 
 
-    "adverbios temporales": [
+    "adverbiosTemporales": [
         { "latin": "adverbios temporales", "japanese": "じかんのふくし" },
         { "latin": "por la mañana", "japanese": "あさ" },
         { "latin": "al mediodía", "japanese": "ひる" },
@@ -105,7 +105,7 @@ var dictionary = {
     ],
 
 
-    "países": [
+    "paises": [
         { "latin": "España", "japanese": "スペイン" },
         { "latin": "Japón", "japanese": "にほん" },
         { "latin": "Italia", "japanese": "イタリア" },
@@ -201,7 +201,7 @@ var dictionary = {
     ],
 
 
-    "vehículos": [
+    "vehiculos": [
         { "latin": "vehículos", "japanese": "のりもの" },
         { "latin": "tren", "japanese": "でんしゃ" },
         { "latin": "metro", "japanese": "ちかてつ" },
@@ -240,6 +240,15 @@ var dictionary = {
 
 var comidas = false;
 var bebidas = false;
+var semana = false;
+var adverbiosTemporales = false;
+var paises = false;
+var profesiones = false;
+var personas = false;
+var cosas = false;
+var lugares = false;
+var vehiculos = false;
+var verbos = false;
 
 function changeComidas() {
     comidas = !comidas;
@@ -249,12 +258,57 @@ function changeBebidas() {
     bebidas = !bebidas;
 }
 
+function changeSemana() {
+    semana = !semana;
+}
+
+function changeAdverbiosTemporales() {
+    adverbiosTemporales = !adverbiosTemporales;
+}
+
+function changePaises() {
+    paises = !paises;
+}
+
+function changeProfesiones() {
+    profesiones = !profesiones;
+}
+
+function changePersonas() {
+    personas = !personas;
+}
+
+function changeCosas() {
+    cosas = !cosas;
+}
+
+function changeLugares() {
+    lugares = !lugares;
+}
+
+function changeVehiculos() {
+    vehiculos = !vehiculos;
+}
+
+function changeVerbos() {
+    verbos = !verbos;
+}
+
 function calculateWordsArray() {
     let words = [];
 
     if( comidas ) { words = words.concat(dictionary.comidas); }
     if( bebidas ) { words = words.concat(dictionary.bebidas); }
-
+    if( semana ) { words = words.concat(dictionary.semana); }
+    if( adverbiosTemporales ) { words = words.concat(dictionary.adverbiosTemporales); }
+    if( paises ) { words = words.concat(dictionary.paises); }
+    if( profesiones ) { words = words.concat(dictionary.profesiones); }
+    if( personas ) { words = words.concat(dictionary.personas); }
+    if( cosas ) { words = words.concat(dictionary.cosas); }
+    if( lugares ) { words = words.concat(dictionary.lugares); }
+    if( vehiculos ) { words = words.concat(dictionary.vehiculos); }
+    if( verbos ) { words = words.concat(dictionary.verbos); }
+    
     return words;
 }
 
@@ -377,6 +431,42 @@ function restart() {
         <div>
             <input type="checkbox" id="bebidas" name="bebidas" ` + (bebidas ? `checked` : `false`) + ` onchange="changeBebidas()">
             <label for="bebidas">Bebidas</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="semana" name="semana" ` + (semana ? `checked` : `false`) + ` onchange="changeSemana()">
+            <label for="semana">D&iactue;as de la semana</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="adverbiosTemporales" name="adverbiosTemporales" ` + (adverbiosTemporales ? `checked` : `false`) + ` onchange="changeAdverbiosTemporales()">
+            <label for="adverbiosTemporales">Adverbios Temporales</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="paises" name="paises" ` + (paises ? `checked` : `false`) + ` onchange="changePaises()">
+            <label for="paises">Pa&iactue;ses</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="profesiones" name="profesiones" ` + (profesiones ? `checked` : `false`) + ` onchange="changeProfesiones()">
+            <label for="profesiones">Profesiones</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="personas" name="personas" ` + (personas ? `checked` : `false`) + ` onchange="changePersonas()">
+            <label for="personas">Personas</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="cosas" name="cosas" ` + (cosas ? `checked` : `false`) + ` onchange="changeCosas()">
+            <label for="cosas">Cosas</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="lugares" name="lugares" ` + (lugares ? `checked` : `false`) + ` onchange="changeLugares()">
+            <label for="lugares">Lugares</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="vehiculos" name="vehiculos" ` + (vehiculos ? `checked` : `false`) + ` onchange="changeVehiculos()">
+            <label for="vehiculos">Veh&iactue;culos</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="verbos" name="verbos" ` + (verbos ? `checked` : `false`) + ` onchange="changeVerbos()">
+            <label for="verbos">Verbos</label><br>
         </div>
     </div>`;
 }
