9fb636e3fffd63fea593b353c1b58846516c9b7c
Morteux <ignacio.melendezlapi@alum.uca.es>
2023-03-12 13:24:11 +0100

    Dictionary - Foods, drinks, weekdays, temporal adverbs, countries

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,16 +1,16 @@
 var dictionary = {
     "comidas": [
         { "latin": "comidas", "japanese": "たべもの" },
-        { "latin": "arroz", "japanese": "ごはん" },
+        { "latin": ["arroz", "comida"], "japanese": "ごはん" },
         { "latin": "pan", "japanese": "パン" },
-        { "latin": "pescado", "japanese": "さかな" },
+        { "latin": ["pescado", "pez"], "japanese": "さかな" },
         { "latin": "carne", "japanese": "にく" },
         { "latin": "carne de pollo", "japanese": "とりにく" },
         { "latin": "carne de vaca", "japanese": "ぎゅうにく" },
         { "latin": "carne de cerdo", "japanese": "ぶたにく" },
         { "latin": "jamón serrano", "japanese": "なまハム" },
         { "latin": "huevo", "japanese": "たまご" },
-        { "latin": "verdura", "japanese": "やさい" },
+        { "latin": "verduras", "japanese": "やさい" },
         { "latin": "tomate", "japanese": "トマト" },
         { "latin": "pepino", "japanese": "きゅうり" },
         { "latin": "repollo", "japanese": "キャベツ" },
@@ -36,27 +36,109 @@ var dictionary = {
         { "latin": "dulces", "japanese": "おかし" },
         { "latin": "caramelo", "japanese": "あめ" },
         { "latin": "chocolate", "japanese": "チョコレート" },
-        { "latin": "tarta", "japanese": "ケーキ" },
+        { "latin": ["tarta", "bizcocho"], "japanese": "ケーキ" },
         { "latin": "galleta", "japanese": "クッキー" },
         { "latin": "patatas fritas en bolsa", "japanese": "ポテトチップス" },
         { "latin": "chicle", "japanese": "ガム" }
     ],
 
 
-    "kanji": [
+    "bebidas": [
+        { "latin": "bebidas", "japanese": "のみもの" },
+        { "latin": "agua", "japanese": "みず" },
+        { "latin": ["té", "té japonés"], "japanese": "おちゃ" },
+        { "latin": "té inglés", "japanese": "こうちゃ" },
+        { "latin": "té con limón", "japanese": "レモンテイー" },
+        { "latin": "té con leche", "japanese": "ミルクテイー" },
+        { "latin": "café", "japanese": "コーヒー" },
+        { "latin": "café con leche", "japanese": "カフェオレ" },
+        { "latin": "leche", "japanese": ["ぎゅうみゅう", "ミルク"] },
+        { "latin": "cola", "japanese": "コーラ" },
+        { "latin": ["zumo", "refresco"], "japanese": "ジュース" },
+        { "latin": "zumo de naranja", "japanese": "オレンジジュース" },
+        { "latin": "zumo de tomate", "japanese": "トマトジュース" },
+        { "latin": "bebida alcohólica", "japanese": "おさけ" },
+        { "latin": "cerveza", "japanese": "ビール" },
+        { "latin": "vino", "japanese": "ワイン" }
+    ],
+
+
+    "días de la semana": [
+        { "latin": "días de la semana", "japanese": "ようび" },
+        { "latin": "lunes", "japanese": "げつようび" },
+        { "latin": "martes", "japanese": "かようび" },
+        { "latin": "miércoles", "japanese": "すいようび" },
+        { "latin": "jueves", "japanese": "もくようび" },
+        { "latin": "viernes", "japanese": "キんようび" },
+        { "latin": "sábado", "japanese": "どようび" },
+        { "latin": "domingo", "japanese": "にちようび" }
+    ],
+
+
+    "adverbios temporales": [
+        { "latin": "adverbios temporales", "japanese": "じかんのふくし" },
+        { "latin": "por la mañana", "japanese": "あさ" },
+        { "latin": "al mediodía", "japanese": "ひる" },
+        { "latin": "por la tarde", "japanese": "ごご" },
+        { "latin": "por la noche", "japanese": ["よう", "ばん"] },
+        { "latin": "hoy", "japanese": "きょう" },
+        { "latin": "mañana", "japanese": "あした" },
+        { "latin": "pasado mañana", "japanese": "あさって" },
+        { "latin": "esta semana", "japanese": "こんしゅう" },
+        { "latin": "la semana que viene", "japanese": "らいしゅう" },
+        { "latin": "el fin de semana", "japanese": "しゅうまつ" },
+        { "latin": "este mes", "japanese": "こんげつ" },
+        { "latin": "el mes que viene", "japanese": "らいげつ" },
+        { "latin": "este año", "japanese": "ことし" },
+        { "latin": "el año que viene", "japanese": "らいねん" },
+        { "latin": "todos los días", "japanese": "まいにち" },
+        { "latin": "todos las mañanas", "japanese": "まいあさ" },
+        { "latin": "todos las noches", "japanese": "まいばん" },
+        { "latin": "todos las semanas", "japanese": "まいしゅう" },
+        { "latin": "todos los meses", "japanese": "まいつき" },
+        { "latin": "todos los años", "japanese": "まいとし" },
+        { "latin": "ayer", "japanese": "きのう" },
+        { "latin": "anteayer", "japanese": "おととい" },
+        { "latin": "la semana pasada", "japanese": "せんしゅう" },
+        { "latin": "el mes pasado", "japanese": "せんげつ" },
+        { "latin": "el año pasado", "japanese": "きょねん" }
+    ],
+
+
+    "países": [
+        { "latin": "España", "japanese": "スペイン" },
+        { "latin": "Japón", "japanese": "にほん" },
+        { "latin": "Italia", "japanese": "イタリア" },
+        { "latin": "Francia", "japanese": "フランス" },
+        { "latin": "Alemania", "japanese": "ドイツ" },
+        { "latin": "Inglaterra", "japanese": "イギリス" },
+        { "latin": "Estados Unidos", "japanese": "アメリカ" },
+        { "latin": "China", "japanese": "ちゅうごく" },
+        { "latin": "Corea", "japanese": "かんこく" },
+        { "latin": "Rusia", "japanese": "ロシア" },
+        { "latin": "Portugal", "japanese": "ポルトガル" },
+        { "latin": "Argentina", "japanese": "アルゼンチン" },
+        { "latin": "Brasil", "japanese": "ブラジルル" },
+        { "latin": "Tailandia", "japanese": "タイ" }
     ]
 };
 
 var comidas = false;
+var bebidas = false;
 
 function changeComidas() {
     comidas = !comidas;
+}
+
+function changeBebidas() {
+    bebidas = !bebidas;
 }
 
 function calculateWordsArray() {
     let words = [];
 
     if( comidas ) { words = words.concat(dictionary.comidas); }
+    if( bebidas ) { words = words.concat(dictionary.bebidas); }
 
     return words;
 }
@@ -111,7 +193,12 @@ function startDictionary() {
 function next() {
     let input = document.getElementById("latin_input").value;
 
-    if (input.toUpperCase() == shuffled[actualIndex].latin.toUpperCase()) {
+    if (!Array.isArray(shuffled[actualIndex].latin) && input.toUpperCase() == shuffled[actualIndex].latin.toUpperCase()) {
+        ++correctCount;
+        $('#img_element').css("background-image", "url('images/correct.png')").fadeIn(0).fadeOut();
+        document.getElementById("latin_input").value = "";
+        document.getElementById("latin_input").placeholder = "";
+    } else if (Array.isArray(shuffled[actualIndex].latin) && shuffled[actualIndex].latin.some(e => e.toUpperCase() === input.toUpperCase())) {
         ++correctCount;
         $('#img_element').css("background-image", "url('images/correct.png')").fadeIn(0).fadeOut();
         document.getElementById("latin_input").value = "";
@@ -171,6 +258,10 @@ function restart() {
         <div>
             <input type="checkbox" id="comidas" name="comidas" ` + (comidas ? `checked` : `false`) + ` onchange="changeComidas()">
             <label for="comidas">Comidas</label><br>
+        </div>
+        <div>
+            <input type="checkbox" id="bebidas" name="bebidas" ` + (bebidas ? `checked` : `false`) + ` onchange="changeBebidas()">
+            <label for="bebidas">Bebidas</label><br>
         </div>
     </div>`;
 }
