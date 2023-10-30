cdbea74adde0309e5edb7315f78b6a613bd2c5bb
IgnacioML <nmelendez@arfuture.es>
2023-10-30 14:24:19 +0100

    Lesson 2 Kanji added

--- json/syllables/kanji.js
+++ json/syllables/kanji.js
@@ -27,8 +27,23 @@ const dictionary = {
     ],
 
     "kanji_lesson_2": [
-        { "yomi": "くんよみ", "kana": "", "kanji": "" },
-        { "yomi": "オンよみ", "kana": "", "kanji": "" }
+        { "yomi": "くんよみ", "kana": "やま", "kanji": "山" },
+        { "yomi": "オンよみ", "kana": "サン", "kanji": "山" },
+        { "yomi": "くんよみ", "kana": "かわ", "kanji": "川" },
+        { "yomi": "くんよみ", "kana": "つき", "kanji": "月" },
+        { "yomi": "オンよみ", "kana": ["ゲツ", "ガツ"], "kanji": "月" },
+        { "yomi": "オンよみ", "kana": "ひ", "kanji": "火" },
+        { "yomi": "くんよみ", "kana": "カ", "kanji": "火" },
+        { "yomi": "オンよみ", "kana": "みず", "kanji": "水" },
+        { "yomi": "くんよみ", "kana": "スイ", "kanji": "水" },
+        { "yomi": "オンよみ", "kana": "かね", "kanji": "金" },
+        { "yomi": "くんよみ", "kana": "キン", "kanji": "金" },
+        { "yomi": "オンよみ", "kana": "つち", "kanji": "土" },
+        { "yomi": "くんよみ", "kana": "ド", "kanji": "土" },
+        { "yomi": "オンよみ", "kana": ["ひ", "び", "か"], "kanji": "日" },
+        { "yomi": "くんよみ", "kana": ["ニチ", "ジツ"], "kanji": "日" },
+        { "yomi": "オンよみ", "kana": "とし", "kanji": "年" },
+        { "yomi": "くんよみ", "kana": "ネン", "kanji": "年" },
     ],
 
     "kanji_lesson_3": [
