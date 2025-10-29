cb364de79ef1784e352bcf118da34686b8b35fd7
Morteux <45830923+Morteux@users.noreply.github.com>
2025-10-29 11:24:57 +0900

    Book important added

---
+++ json/bookImportant.js
@@ -0,0 +1,151 @@
+const book_json = {
+    "days": [
+        { "spanish": "domingo", "kana": "にちようび", "kanji": "日曜日" },
+        { "spanish": "Japón", "kana": "にほん", "kanji": "日本" },
+        { "spanish": "Japón", "kana": "にっぽん", "kanji": "日本" },
+
+        { "spanish": "lunes", "kana": "げつようび", "kanji": "月曜日" },
+        { "spanish": "próximo mes", "kana": "らいげつ", "kanji": "来月" },
+
+        { "spanish": "fuego", "kana": "ひ", "kanji": "火" },
+        { "spanish": "martes", "kana": "かようび", "kanji": "火曜日" },
+        
+        { "spanish": "agua", "kana": "みず", "kanji": "水" },
+        { "spanish": "miércoles", "kana": "すいようび", "kanji": "水曜日" },
+
+        { "spanish": "árbol", "kana": "き", "kanji": "木" },
+        { "spanish": "jueves", "kana": "もくようび", "kanji": "木曜日" },
+
+        { "spanish": "dinero", "kana": "おかね", "kanji": "お金" },
+        { "spanish": "viernes", "kana": "きんようび", "kanji": "金曜日" },
+
+        { "spanish": "tierra", "kana": "つち", "kanji": "土" },
+        { "spanish": "sábado", "kana": "どようび", "kanji": "土曜日" },
+
+        { "spanish": "domingo", "kana": "にちようび", "kanji": "日曜日" },
+        { "spanish": "lunes", "kana": "げつようび", "kanji": "月曜日" },
+    ],
+
+    "numbers_1": [
+        { "spanish": "uno", "kana": "ひとつ", "kanji": "一つ" },
+        { "spanish": "enero", "kana": "いちがつ", "kanji": "一月" },
+        
+        { "spanish": "dos", "kana": "ふたつ", "kanji": "二つ" },
+        { "spanish": "febrero", "kana": "にがつ", "kanji": "二月" },
+        
+        { "spanish": "tres", "kana": "みっつ", "kanji": "三つ" },
+        { "spanish": "marzo", "kana": "さんがつ", "kanji": "三月" },
+        
+        { "spanish": "abril", "kana": "しがつ", "kanji": "四月" },
+        { "spanish": "cuatro", "kana": "よっつ", "kanji": "四つ" },
+        
+        { "spanish": "día 5", "kana": "いつか", "kanji": "五日" },
+        { "spanish": "cinco cosas", "kana": "いつつ", "kanji": "五つ" },
+        
+        { "spanish": "día 6", "kana": "むいか", "kanji": "六日" },
+        { "spanish": "seis", "kana": "むっつ", "kanji": "六つ" },
+        
+        { "spanish": "día 7", "kana": "なのか", "kanji": "七日" },
+        { "spanish": "siete", "kana": "ななつ", "kanji": "七つ" },
+
+        { "spanish": "ocho", "kana": "やっつ", "kanji": "八つ" },
+        { "spanish": "día 8", "kana": "ようか", "kanji": "八日" },
+    ],
+
+    "numbers_2": [
+        { "spanish": "nueve", "kana": "ここのつ", "kanji": "九つ" },
+        { "spanish": "septiembre", "kana": "くがつ", "kanji": "九月" },
+
+        { "spanish": "diez", "kana": "じゅう", "kanji": "十" },
+        { "spanish": "día 10", "kana": "とおか", "kanji": "十日" },
+        
+        { "spanish": "cien", "kana": "ひゃく", "kanji": "百" },
+        { "spanish": "cien yenes", "kana": "ひゃくえん", "kanji": "百円" },
+        
+        { "spanish": "distrito de Chiyoda", "kana": "ちよだく", "kanji": "千代田区" },
+        { "spanish": "mil", "kana": "せん", "kanji": "千" },
+        
+        { "spanish": "pluma estilográfica", "kana": "まんねんひつ", "kanji": "万年筆" },
+        { "spanish": "billete de diez mil yenes", "kana": "いちまんえんさつ", "kanji": "一万円札" },
+        
+        { "spanish": "mitad", "kana": "はんぶん", "kanji": "半分" },
+        { "spanish": "doce y media", "kana": "じゅうにじはん", "kanji": "十二時半" },
+        
+        { "spanish": "total", "kana": "ぜんぶ", "kanji": "全部" },
+        { "spanish": "seguro", "kana": "あんぜん", "kanji": "安全" },
+        
+        { "spanish": "contar", "kana": "かぞえる", "kanji": "数える" },
+        { "spanish": "matemáticas", "kana": "すうがく", "kanji": "数学" },
+    ],
+
+    "money": [
+        { "spanish": "redondo", "kana": "まるい", "kanji": "円い" },
+        { "spanish": "un yen", "kana": "いちえん", "kanji": "一円" },
+        
+        { "spanish": "padres", "kana": "りょうしん", "kanji": "両親" },
+        { "spanish": "ambos", "kana": "りょうほう", "kanji": "両方" },
+        
+        { "spanish": "intercambio", "kana": "かえる", "kanji": "替える" },
+        { "spanish": "intercambio de dinero", "kana": "りょうがえ", "kanji": "両替" },
+        
+        { "spanish": "cargamento", "kana": "かもつ", "kanji": "貨物" },
+        { "spanish": "moneda", "kana": "こうか", "kanji": "硬貨" },
+        
+        { "spanish": "aduanas", "kana": "ぜいかん", "kanji": "税関" },
+        { "spanish": "impuesto", "kana": "ぜいきん", "kanji": "税金" },
+        
+        { "spanish": "licencia", "kana": "めんきょう", "kanji": "免許" },
+        { "spanish": "libre de impuestos", "kana": "めんぜい", "kanji": "免税" },
+        
+        { "spanish": "consulado", "kana": "りょうじかん", "kanji": "領事館" },
+        { "spanish": "recibo", "kana": "りょうしゅうしょう", "kanji": "領収証" },
+        
+        { "spanish": "obtener", "kana": "おさめる", "kanji": "収める" },
+        { "spanish": "ingresos", "kana": "しゅうにゅう", "kanji": "収入" },
+        
+        { "spanish": ["certificación", "verificación"], "kana": "しょうめい", "kanji": "証明" },
+        { "spanish": "identificación de estudiante", "kana": "がくせいしょう", "kanji": "学生証" },
+    ],
+
+    "time": [
+        { "spanish": "treinta minutos", "kana": ["さんじっぷん", "さんじゅっぷん"], "kanji": "30分" },
+        { "spanish": "uno mismo", "kana": "じぶん", "kanji": "自分" },
+        
+        { "spanish": "a veces", "kana": "ときどき", "kanji": "時々" },
+        { "spanish": ["tiempo", "hora"], "kana": "じかん", "kanji": "時間" },
+        
+        { "spanish": "esta semana", "kana": "こんしゅう", "kanji": "今週" },
+        { "spanish": "la semana que viene", "kana": "らいしゅう", "kanji": "来週" },
+        
+        { "spanish": ["edad", "años"], "kana": "ねんれい", "kanji": "年齢" },
+        { "spanish": "estudiante de primer grado", "kana": "いちねんせい", "kanji": "1年生" },
+        
+        { "spanish": ["ahora", "presente"], "kana": "いま", "kanji": "今" },
+        { "spanish": "este mes", "kana": "こんげつ", "kanji": "今月" },
+        
+        { "spanish": "la primera vez", "kana": "はじめて", "kanji": "初めて" },
+        { "spanish": "comienzo", "kana": "さいしょ", "kanji": "最初" },
+        
+        { "spanish": ["inicio", "comienzo"], "kana": "はじめる", "kanji": "始める" },
+        { "spanish": ["inicio", "principio"], "kana": "かいし", "kanji": "開始" },
+        
+        { "spanish": ["final", "completo"], "kana": "おわる", "kanji": "終わる" },
+        { "spanish": "último", "kana": "さいしゅう", "kanji": "最終" },
+    ],
+
+    "interrogative_counters": [
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+        { "spanish": "", "kana": "", "kanji": "" },
+    ],
+};
